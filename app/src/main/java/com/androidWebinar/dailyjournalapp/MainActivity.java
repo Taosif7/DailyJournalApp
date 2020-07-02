fedb0bfd2f767b380506c6140d5f956bcb1ecaef
Taosif7 <taosif.7@gmail.com>
2020-07-02 13:53:08 +0530

    > Added FAB in MainActivity

--- app/src/main/java/com/androidWebinar/dailyjournalapp/MainActivity.java
+++ app/src/main/java/com/androidWebinar/dailyjournalapp/MainActivity.java
@@ -1,14 +1,32 @@
 package com.androidWebinar.dailyjournalapp;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set Layout
         setContentView(R.layout.activity_main);
+
+        // Find Views
+        FloatingActionButton newJournalBtn = findViewById(R.id.btn_add_journal);
+
+        // Add Listener to FAB
+        newJournalBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO : Launch the new Journal Activity using intent
+                Log.d("FAB_LOG", "Button Clicked");
+            }
+        });
     }
 }
